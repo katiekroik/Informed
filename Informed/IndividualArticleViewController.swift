c052435115bd2297733e857aa23f5505a8a3a61d
katiekroik <7katie@gmail.com>
2016-04-04 16:36:08 -0400

    added some code for seeing if the user scrolled in the individual article controller

--- Informed/IndividualArticleViewController.swift
+++ Informed/IndividualArticleViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class IndividualArticleViewController: UIViewController {
-
     
     // Name of article
     @IBOutlet weak var articleName: UILabel!
     // Article contents
     @IBOutlet weak var article: UIScrollView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        article.decelerationRate = UIScrollViewDecelerationRateFast;
+        NSLog("Did load Individual Article");
+        scrollViewDidEndDragging(article, willDecelerate:false);
+
         // Do any additional setup after loading the view.
+    }
+    
+    func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>)
+    {
+        //This is the index of the "page" that we will be landing at
+        let nearestIndex = Int(CGFloat(targetContentOffset.memory.x) / scrollView.bounds.size.width + 0.5)
+        
+        //Just to make sure we don't scroll past your content
+        let clampedIndex = 100.5; // max( min(nearestIndex, yourPagesArray.count - 1 ), 0 )
+        
+        NSLog("hi");
+        //This is the actual x position in the scroll view
+        var xOffset = CGFloat(clampedIndex) * scrollView.bounds.size.width
+        
+        //I've found that scroll views will "stick" unless this is done
+        xOffset = xOffset == 0.0 ? 1.0 : xOffset
+        
+        //Tell the scroll view to land on our page
+        targetContentOffset.memory.x = xOffset
+        
+        
+    }
+    
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool)
+    {
+        NSLog("Called scrollViewDidEndDragging");
+        if !decelerate
+        {
+            
+            let currentIndex = floor(scrollView.contentOffset.x / scrollView.bounds.size.width);
+            // NSLog("%@", currentIndex);
+            let offset = CGPointMake(scrollView.bounds.size.width * currentIndex, 0)
+            
+            scrollView.setContentOffset(offset, animated: true)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,7 +65,10 @@ class IndividualArticleViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    /*
+        Check how fast the scrolling is
+    */
+    
     /*
     // MARK: - Navigation
 
