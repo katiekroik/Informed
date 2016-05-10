16fffc81b31302761acd321202d7a3d74e5558fc
katiekroik <7katie@gmail.com>
2016-05-10 11:02:12 -0400

    individual article adding points on load instead -> sorry!

--- Informed/IndividualArticleViewController.swift
+++ Informed/IndividualArticleViewController.swift
@@ -10,33 +10,34 @@ import UIKit
 import RealmSwift
 import WebKit
 
-class IndividualArticleViewController: UIViewController, UITextViewDelegate, WKNavigationDelegate {
+class IndividualArticleViewController: UIViewController, UITextViewDelegate, WKNavigationDelegate, UIScrollViewDelegate {
     
-    var article: Article!
     var aUrl: String!
+    var article: Article!
     var currentUser: User!
     var realm = try! Realm()
     var webView: WKWebView!
     
     var alreadyPassedThreshold = false
+
     
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
-        webView.scrollView.delegate = self
+//        webView.scrollView.delegate = self
         view = webView
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "+", style: .Plain, target: self, action: #selector(IndividualArticleViewController.options))
+
         let url = NSURL (string: aUrl);
         let requestObj = NSURLRequest(URL: url!);
         webView.loadRequest(requestObj);
         
-        // If there is no current users, get one
+        
+        var contains = false;
+        
         if currentUser == nil {
             let facebookId = FBSDKAccessToken.currentAccessToken().userID
             let potentialUsers = realm.objects(User).filter("facebookId == %s", facebookId)
@@ -47,11 +48,59 @@ class IndividualArticleViewController: UIViewController, UITextViewDelegate, WKN
             currentUser = potentialUsers.first
         }
         
+        // Keep track of the genre and publishers of the article read
+        var numReadOfGenre = [String: Int]()
+        var numReadOfSource = [String: Int]()
+        
+        for a in currentUser.articlesRead {
+            // If it's a new genre, add it
+            if numReadOfGenre[a.genre.name] == nil {
+                numReadOfGenre[a.genre.name] = 1
+            } else {
+                numReadOfGenre[a.genre.name] = numReadOfGenre[a.genre.name]! + 1
+            }
+            // If it's a new source, add it
+            if numReadOfSource[a.publisher] == nil {
+                numReadOfSource[a.publisher] = 1
+            } else {
+                numReadOfSource[a.publisher] = numReadOfSource[a.publisher]! + 1
+            }
+            
+            if (a.linkTo == aUrl) {
+                contains = true;
+            }
+        }
+        
+        // Create a multiplier based on how many articles you've read from that publisher
+        var mult = max(0.6, 1.2 - Double(numReadOfSource[article.publisher]!))
+        
+        // If the user hasn't read the article
+        if (contains == false) {
+            // Get the new val
+            let val = currentUser.points + Int(max(15, mult * Double(50 - (2 * numReadOfGenre[article.genre.name]!))))
+            print(val)
+            // Add to the user read article database
+            try! realm.write {
+                currentUser.articlesRead.append(article)
+                currentUser.points = val
+            }
+        }
+        else {
+            print("Re-reading an article -> no points...")
+        }
+        
     }
-
-
-    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool){
-        if !decelerate {
+    
+    override func viewWillAppear(animated: Bool) {
+        //        articleName.text = aName;
+    }
+    
+    
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool)
+    {
+        if !decelerate
+        {
+            
             let currentIndex = floor(scrollView.contentOffset.x / scrollView.bounds.size.width);
             let offset = CGPointMake(scrollView.bounds.size.width * currentIndex, 0)
             scrollView.setContentOffset(offset, animated: true)
@@ -60,55 +109,96 @@ class IndividualArticleViewController: UIViewController, UITextViewDelegate, WKN
     
     func scrollViewDidScroll(scrollView: UIScrollView) {
         var contains = false
-        // If the threshold wasn't already passed -> why waste time if it already was
         if (!alreadyPassedThreshold) {
-            // When the offset gets passed 2/3 down the page
-            if (scrollView.contentOffset.y > ((scrollView.contentSize.height - scrollView.frame.size.height) * (2/3))) {
-                alreadyPassedThreshold = true
-                // Keep track of the genre and publishers of the article read
-                var numReadOfGenre = [String: Int]()
-                var numReadOfSource = [String: Int]()
-                
-                for a in currentUser.articlesRead {
-                    // If it's a new genre, add it
-                    if numReadOfGenre[a.genre.name] == nil {
-                        numReadOfGenre[a.genre.name] = 1
-                    } else {
-                        numReadOfGenre[a.genre.name] = numReadOfGenre[a.genre.name]! + 1
-                    }
-                    // If it's a new source, add it
-                    if numReadOfSource[a.publisher] == nil {
-                        numReadOfSource[a.publisher] = 1
-                    } else {
-                        numReadOfSource[a.publisher] = numReadOfSource[a.publisher]! + 1
-                    }
-                    
-                    if (a.linkTo == aUrl) {
-                        contains = true;
-                    }
+            var numReadOfGenre = [String: Int]()
+            var numReadOfSource = [String: Int]()
+            
+            for a in currentUser.articlesRead {
+                // If it's a new genre, add it
+                if numReadOfGenre[a.genre.name] == nil {
+                    numReadOfGenre[a.genre.name] = 1
+                } else {
+                    numReadOfGenre[a.genre.name] = numReadOfGenre[a.genre.name]! + 1
+                }
+                // If it's a new source, add it
+                if numReadOfSource[a.publisher] == nil {
+                    numReadOfSource[a.publisher] = 1
+                } else {
+                    numReadOfSource[a.publisher] = numReadOfSource[a.publisher]! + 1
                 }
                 
-                // Create a multiplier based on how many articles you've read from that publisher
-                var mult = max(0.6, 1.2 - Double(numReadOfSource[article.publisher]!))
-                
-                // If the user hasn't read the article
-                if (contains == false) {
-                    // Get the new val
-                    let val = currentUser.points + Int(max(15, mult * Double(50 - (2 * numReadOfGenre[article.genre.name]!))))
-                    print(val)
-                    // Add to the user read article database
-                    try! realm.write {
-                        currentUser.articlesRead.append(article)
-                        currentUser.points = val
-                    }
-                }
-                // Otherwise, print that you don't get any points
-                else {
-                    print("Re-reading an article -> no points...")
+                if (a.linkTo == aUrl) {
+                    contains = true;
                 }
             }
-
+            
+            // Create a multiplier based on how many articles you've read from that publisher
+            var mult = max(0.6, 1.2 - Double(numReadOfSource[article.publisher]!))
+            
+            // If the user hasn't read the article
+            if (contains == false) {
+                // Get the new val
+                let val = currentUser.points + Int(max(15, mult * Double(50 - (2 * numReadOfGenre[article.genre.name]!))))
+                print(val)
+                // Add to the user read article database
+                try! realm.write {
+                    currentUser.articlesRead.append(article)
+                    currentUser.points = val
+                }
+            }
+            else {
+                print("Re-reading an article -> no points...")
+            }
+            print(currentUser)
+            alreadyPassedThreshold = true
         }
+//        print (scrollView.contentSize.height - scrollView.frame.size.height)
+//        print( scrollView.contentOffset.y )
+//        if (scrollView.contentOffset.y > ((scrollView.contentSize.height - scrollView.frame.size.height) / 2)) {
+//            // reached the bottom
+//            print ("BOTTOM!!")
+//            
+//            var contains = false;
+//            
+//            var numReadOfGenre = [String: Int]()
+//            numReadOfGenre["Politics"] = 0
+//            numReadOfGenre["Sports"] = 0
+//            numReadOfGenre["Entertainment"] = 0
+//            numReadOfGenre["Tech"] = 0
+//            // 0 = Politics, 1 = Sports, 2 = Entertainment, 3 = Tech
+//            
+//            if currentUser == nil {
+//                let facebookId = FBSDKAccessToken.currentAccessToken().userID
+//                let potentialUsers = realm.objects(User).filter("facebookId == %s", facebookId)
+//                if potentialUsers.count == 0 {
+//                    navigationController?.popToRootViewControllerAnimated(true)
+//                    
+//                }
+//                currentUser = potentialUsers.first
+//            }
+//            
+//            for a in currentUser.articlesRead {
+//                //                numReadOfGenre["Politics"]
+//                print (numReadOfGenre["Politics"])
+//                if (a.linkTo == aUrl) {
+//                    contains = true;
+//                }
+//            }
+//            
+//            if (contains == false) {
+//                let val = currentUser.points + 25
+//                // Add to the user read article database
+//                try! realm.write {
+//                    currentUser.articlesRead.append(article)
+//                    currentUser.points = val
+//                }
+//            }
+//            else {
+//                print("Re-reading an article -> no points...")
+//            }
+//            
+//            print(currentUser)
+//        }
     }
     
     func options() {
@@ -124,23 +214,27 @@ class IndividualArticleViewController: UIViewController, UITextViewDelegate, WKN
         }
     }
     
+    func scrollToBotom() {
+        //        let range = NSMakeRange(articleContents.text.characters.count - 1, 1);
+        //        articleContents.scrollRangeToVisible(range);
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     /*
-        Check how fast the scrolling is
+    Check how fast the scrolling is
     */
     
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
